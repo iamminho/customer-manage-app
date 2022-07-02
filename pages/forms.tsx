be7abe02f03f9f0f829f78a80507bf57ba291be7
iamminho <minho100227@kakao.com>
2022-07-02 13:39:46 +0900

    make forms by React

---
+++ pages/forms.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+export default function Forms() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onUsernameChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setUsername(value);
+  };
+  const onEmailChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setEmail(value);
+  };
+  const onPasswordChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setPassword(value);
+  };
+  const onSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault(); // submit 이후에도 form의 내용 보존
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={username}
+        onChange={onUsernameChange}
+        type="text"
+        placeholder="Username"
+        required
+      />
+      <input
+        value={email}
+        onChange={onEmailChange}
+        type="email"
+        placeholder="Email"
+        required
+      />
+      <input
+        value={password}
+        onChange={onPasswordChange}
+        type="password"
+        placeholder="Password"
+        required
+      />
+      <input type="submit" value="Create Account" />
+    </form>
+  );
+}
