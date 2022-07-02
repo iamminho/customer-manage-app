c8f0bfc32ae450131a8feaa4b5324c54a457d82e
iamminho <minho100227@kakao.com>
2022-07-02 17:52:11 +0900

    react-hook-form validation

--- pages/form2.tsx
+++ pages/form2.tsx
@@ -5,26 +5,41 @@ import { useForm } from "react-hook-form";
 // Better validation
 // Better Errors (set, clear, display)
 // Have control over inputs
-// Don't deal with events
+// Don't deal with events (v)
 // Easier Inputs (v)
 
+// 기능: Validation
+// required를 사용하려면 onSubmit에 handleSubmit을 넣어줘야한다.
+// handleSubmit은 매개변수로 받는 또다른 함수를 실행시킨다.
+// requried된것들의 조건을 만족하면 매개변수함수(onValid)가 실행된다.
+// requried의 조건을 만족하지 못하면 만족하지 못한 input에 커서가 자동으로 이동한다.
+
 export default function Forms() {
-  const { register, watch } = useForm();
+  const { register, handleSubmit } = useForm();
   // register = input과 state를 연결시켜준다.
+  const onValid = () => {
+    console.log("im valid bby");
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid)}>
       <input
-        {...register("username")}
+        {...register("username", {
+          required: true,
+        })}
         type="text"
         placeholder="Username"
-        required
       />
-      <input {...register("email")} type="email" placeholder="Email" required />
       <input
-        {...register("password")}
+        {...register("email", { required: true })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", {
+          required: true,
+        })}
         type="password"
         placeholder="Password"
-        required
       />
       <input type="submit" value="Create Account" />
     </form>
