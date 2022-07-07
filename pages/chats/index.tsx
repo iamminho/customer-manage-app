0e3a92f1356411a7dca2c346400cf2301c38c6ed
iamminho <minho100227@kakao.com>
2022-07-07 21:01:17 +0900

    layout two

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,23 +1,29 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Chats: NextPage = () => {
   return (
-    <div className="py-10 divide-y-[1px]">
-      {[1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-          className="flex cursor-pointer py-3 px-4 items-center space-x-3"
-        >
-          <div className="w-12 h-12 rounded-full bg-slate-300" />
-          <div>
-            <p className="font-medium text-gray-700">오리</p>
-            <p className="text-xs font-medium text-gray-500">
-              내일 2시에 사거리에서 봐요
-            </p>
+    <Layout title="대화" hasTabBar>
+      <div className="pb-16 divide-y-[1px]">
+        {[
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+          1, 1,
+        ].map((_, i) => (
+          <div
+            key={i}
+            className="flex cursor-pointer py-3 px-4 items-center space-x-3"
+          >
+            <div className="w-12 h-12 rounded-full bg-slate-300" />
+            <div>
+              <p className="font-medium text-gray-700">오리</p>
+              <p className="text-xs font-medium text-gray-500">
+                내일 2시에 사거리에서 봐요
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import Layout from "../components/layout";
 
 const Home: NextPage = () => {
   return (
     <Layout title="home" hasTabBar>
-      <div className="px-4 flex flex-col space-y-5 py-10">
-        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+      <div className="px-4 flex flex-col space-y-5 pb-10">
+        {[
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ].map((_, i) => (
           <div
             key={i}
             className="flex border-b pb-4 cursor-pointer justify-between"
@@ -58,26 +61,28 @@ const Home: NextPage = () => {
             </div>
           </div>
         ))}
-        <button
-          className="fixed bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-4 text-white
+        <Link href="/items/upload">
+          <button
+            className="fixed bottom-[120px] right-5 shadow-xl bg-orange-400 rounded-full p-4 text-white
       hover:bg-orange-500 cursor-pointer transition-colors"
-        >
-          <svg
-            className="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-            />
-          </svg>
-        </button>
+            <svg
+              className="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+          </button>
+        </Link>
       </div>
     </Layout>
   );
