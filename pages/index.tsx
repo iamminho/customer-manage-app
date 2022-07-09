999f84807df49cf9e9ea40649321d72fdbb7be4f
iamminho <minho100227@kakao.com>
2022-07-09 14:53:45 +0900

    components/item

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Layout from "../components/layout";
 import FloatingButton from "../components/floating-button";
 import Link from "next/link";
+import Item from "../components/item";
 
 const Home: NextPage = () => {
   return (
@@ -10,56 +11,14 @@ const Home: NextPage = () => {
         {[
           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
         ].map((_, i) => (
-          <Link href={`/items/${i}`} key={i}>
-            <div className="flex border-b pb-4 cursor-pointer justify-between">
-              <div className="flex space-x-4">
-                <div className="w-20 h-20 bg-gray-400 rounded-md" />
-                <div className="pt-2 flex flex-col ">
-                  <h3 className="text-sm font-medium text-gray-700">
-                    New iPhone 14
-                  </h3>
-                  <span className="text-xs text-gray-400">Black</span>
-                  <span className="font-medium mt-1 text-gray-900">$95</span>
-                </div>
-              </div>
-              <div className="flex space-x-1.5 items-end justify-end">
-                <div className="flex space-x-0.5 items-center text-sm">
-                  <svg
-                    className="w-4 h-4"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                    ></path>
-                  </svg>
-                  <span>1</span>
-                </div>
-                <div className="flex space-x-0.5 items-center text-sm">
-                  <svg
-                    className="w-4 h-4"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                    ></path>
-                  </svg>
-                  <span>1</span>
-                </div>
-              </div>
-            </div>
-          </Link>
+          <Item
+            id={i}
+            key={i}
+            title="iphone 14"
+            price={99}
+            comments={1}
+            hearts={1}
+          />
         ))}
         <FloatingButton href="/items/upload">
           <svg
